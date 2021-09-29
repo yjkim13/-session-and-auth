b59ff08553b7d51f83d93643a712741db3ab68b3
yjkim13 <sk940913@naver.com>
2021-09-29 18:42:16 +0900

    session store install

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var parseurl = require('parseurl')
 var session = require('express-session')
+var FileStore = require('session-file-store')(session);
 
 var app = express()
 
 app.use(session({
   secret: 'keyboard cat',
   resave: false, // session 데이터가 바뀌기전까지는 저장소를 바뀌지 않는다. 트루면, 바뀌었건 안바뀌었건 바뀌지 않는다.
-  saveUninitialized: true // 세션이 필요하기 전까지 세션을 구동시키지 않는다.
+  saveUninitialized: true, // 세션이 필요하기 전까지 세션을 구동시키지 않는다.
+  store: new FileStore()
 }))
 
 app.get('/', function (req, res, next) {
